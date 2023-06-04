cec8a0a1ab8bed4f289db69845a112cef3b63d9d
Mumtahina Nur <mumtahinanur9@gmail.com>
2023-06-04 18:50:20 +0600

    created
    description

---
+++ increasingArray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, i, move = 0;
+    cin >> n;
+    long long a[n];
+
+    for(i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for(i = 0; i < n-1; i++) {
+        if(a[i] > a[i+1]) {
+            move += (a[i] - a[i+1]);
+            a[i+1] = a[i];
+        }
+    }
+
+    cout << move;
+
+    return 0;
+}
